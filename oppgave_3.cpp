712e8181652cff73d3da9dad7c344055c8fbe786
Divielva <Sebaslett@gmail.com>
2021-11-07 23:19:32 +0100

    Oppgave 3 (Ordentlig satt opp)
    Satt inn riktig kode og fullført oppgaven
    la til en print funksjon også for å bare vise outputtet etterpå

--- oppgave_3.cpp
+++ oppgave_3.cpp
@@ -9,48 +9,24 @@ Oppgave_3::Oppgave_3()
 
 void Oppgave_3::Aktiver()
 {
-    obg3_unordered_set();
 
+    //we set up unordered set
+    std::unordered_set<Test> us;
+    std::cout << "Unordered_set in specified order" << std::endl;
+
+        //We make a small array with the numbers within
+        int a[10] {12, 13, 20, 21, 1, 2, 3, 4, 5, 6};
+        for (int i= 0; i<10; i++)
+        {
+            Test t;
+            t.key = a[i];
+            // dette setter a[i] = 0 til 'a' i stringen.
+            t.s = 97+a[i];
+            us.insert(t);
+            //We print out the array numbers in the same order
+            std::cout << " " << t.key << " ";
+        }
+        std::cout << std::endl;
 }
 
-void obg3_unordered_set ()
-{
-    hash<Test> ht;
-    unordered_set<Test> us;
-    unordered_set<Test>::iterator usit;
-    pair<unordered_set<Test>::iterator, bool> par;
-
-    Test t;
-
-
-    std::cout << "Tester Hash Funksjonen" << std::endl;
-    for (auto i= 0; i<15; i++)
-    {
-        t.key = i;
-        std::cout << "Hash ht (" << i << ") = " << ht(t) << std::endl;
-    }
-
-    t.key = 12; t.s = "Tolv";
-    us.insert(t);
-    t.key = 14; t.s = "fjorten";
-    us.insert(t);
-
-    t.key = 14; t.s = "Fjorten b";
-    int m = us.size();
-
-    par = us.insert(t);
-    int n = us.size();
-
-    std::cout << t.s << " satt inn? " << boolalpha << ( n=m==1) << std::endl;
-
-    std::cout << t.s << " satt inn? " << boolalpha << par.second << std::endl;
-
-    t.key = 14;
-    usit = us.find(t);
-    t = *usit;
-    std::cout << " " << std::endl;
-    std::cout << "post funnet: " << t.key << " " << t.s << std::endl;
-
-
-}
 
