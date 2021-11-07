f4a742f2ca8d0df19c6c3141afcbbb6afde472c9
yretes <69961361+yretes@users.noreply.github.com>
2021-11-07 21:07:24 +0100

    Gjort oppgave 4 A og B, gjort forsøk på Dijkstra's

--- oppgave_4.cpp
+++ oppgave_4.cpp
@@ -7,5 +7,115 @@ Oppgave_4::Oppgave_4()
 
 void Oppgave_4::Aktiver()
 {
+    cout<<"oppg 4: "<<endl;
+    Graf oppg4Graf;
+    oppg4Graf.settinn_node('A');
+    oppg4Graf.settinn_node('B');
+    oppg4Graf.settinn_node('C');
+    oppg4Graf.settinn_node('D');
+    oppg4Graf.settinn_node('E');
 
+    oppg4Graf.settinn_kant('A','B',1.0);
+    oppg4Graf.settinn_kant('A','C',2.0);
+    oppg4Graf.settinn_kant('B','C',2.0);
+    oppg4Graf.settinn_kant('C','D',3.0);
+    oppg4Graf.settinn_kant('D','E',1.0);
+    oppg4Graf.settinn_kant('A','E',5.0);
+    oppg4Graf.settinn_kant('C','E',4.0);
+
+    // kontroll testing //
+    for(Node* n : oppg4Graf.noder){
+        cout<<n->m_navn<<" kanter: "<<endl;
+        for (Kant k :n->m_kanter)
+            cout<<n->m_navn<<k.m_tilnode->m_navn<<" med vekt = "<<k.m_vekt<<endl;
+
+    }
+    // får ikke Dijkstras algoritme til å funke
+    //cout<<endl<<"dijkstra test";
+    //Veg FerdigVeg=DijkstraAlgo(oppg4Graf,'A','D');
+
+    //for(Node* n : FerdigVeg.Noder)
+      //  cout<<n->m_navn;
+}
+
+//Oppgave_4::Veg Oppgave_4::DijkstraAlgo(Graf graf, char startNavn, char sluttNavn)
+//{
+//    Node* start;
+//    Node* slutt;
+//    for(Node* n:graf.noder){
+//        if(n->m_navn==startNavn)
+//          start=n;
+//        if(n->m_navn==sluttNavn)
+//            slutt=n;
+
+//    }
+
+//    list<Veg> Veger;
+//    list<Veg> FerdigeVeger;
+//    for (Kant k : start->m_kanter)
+//        Veger.emplace_back(new Veg(start,k.m_vekt));
+
+//    for(Veg v: Veger){
+//            if(v.rekkerSlutt){
+//                for(Node* n: v.Noder){
+//                    if(n->m_kanter.size()<1&&n->m_navn!=slutt->m_navn){
+//                        v.rekkerSlutt=false;
+//                    }
+//                    else if(n->m_navn==slutt->m_navn){
+//                        FerdigeVeger.emplace_back(v);
+//                    }
+//                    else
+//                        for(Kant k : n->m_kanter){
+//                           Veger.emplace_back(new Veg(start,v.m_prioritetsVekt+k.m_vekt));
+//                        }
+//                }
+//            }
+//        }
+
+//        for(Veg v : FerdigeVeger){
+//            for(Veg f :FerdigeVeger){
+//                if(f.Noder!=v.Noder&&f.m_prioritetsVekt<v.m_prioritetsVekt){
+//                    Veg TempVeg=f;
+//                    f=v;
+//                    v=TempVeg;
+//                }
+//            }
+//        }
+
+//        return FerdigeVeger.front();
+
+//}
+
+void Oppgave_4::Node::settinn_kant(const Kant &kant)
+{
+    Node::m_kanter.push_back(kant);
+}
+
+Oppgave_4::Node *Oppgave_4::Graf::finn_node(char navn)
+{
+    for(Node* n : noder){
+        if(n->m_navn==navn)
+            return n;
+    }
+    return nullptr;
+}
+
+void Oppgave_4::Graf::settinn_node(char navn)
+{
+    Node* nyNode= new Node(navn);
+    Graf::noder.emplace_back(nyNode);
+}
+
+void Oppgave_4::Graf::settinn_kant(char fra_navn, char til_navn, float vekt)
+{
+    for(Node* n : noder){
+        if(n->m_navn==fra_navn){
+            for (Node* m : noder){
+                if(m->m_navn==til_navn){
+                    Kant nyKant(vekt,m);
+                    n->m_kanter.emplace_back(nyKant);
+                }
+            }
+        }
+    }
 }
--- oppgave_4.h
+++ oppgave_4.h
@@ -1,13 +1,55 @@
 #ifndef OPPGAVE_4_H
 #define OPPGAVE_4_H
-
+#include <iostream>
+#include <list>
+#include <queue>
+using namespace std;
 
 class Oppgave_4
 {
 public:
     Oppgave_4();
     void Aktiver();
-    int memes =1;
+
+    struct Kant;
+    struct Node{
+        char m_navn;
+        bool m_besokt;
+        std::list<Kant> m_kanter;
+        Node(char navn) : m_navn(navn), m_besokt(false) { }
+        void settinn_kant(const Kant &kant);
+    };
+
+    struct Kant {
+        float m_vekt;
+        Node* m_tilnode;
+        Kant(float vekt, Node* tilnode) : m_vekt(vekt), m_tilnode(tilnode){ }
+        bool operator > (const Kant& k) const {return m_vekt > k.m_vekt;}
+    };
+
+    struct Graf{
+        std::list<Node*> noder;
+        Graf() {}
+        Node* finn_node(char navn);
+        void settinn_node(char navn);
+        void settinn_kant(char fra_navn, char til_navn, float vekt);
+        float mst();
+    };
+
+    // Dijkstra's //
+
+    struct Veg{
+
+        Node* m_start;
+        float m_prioritetsVekt;
+        bool rekkerSlutt;
+        Veg(Node* start, float prioritetsVekt): m_start(start), m_prioritetsVekt(prioritetsVekt),
+            rekkerSlutt(true){ }
+        std::list<Node*> Noder;
+
+    };
+
+    Veg DijkstraAlgo(Graf graf, char startNavn, char sluttNavn);
 };
 
 #endif // OPPGAVE_4_H
